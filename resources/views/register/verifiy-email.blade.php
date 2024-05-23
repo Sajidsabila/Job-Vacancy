70b95785d8fe8aac90c073d352ec9353ce87a091
Sajidsabila <sajid.sabila@gmail.com>
2024-05-23 13:12:09 +0700

    add registrration

---
+++ resources/views/register/verifiy-email.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta name="author" content="Muhamad Nauval Azhar">
+	<meta name="viewport" content="width=device-width,initial-scale=1">
+	<meta name="description" content="This is a login page template based on Bootstrap 5">
+	<title>Job Vacancy | Register Page</title>
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+</head>
+
+<body>
+	<section class="h-100">
+		<div class="container h-100">
+			<div class="row justify-content-sm-center h-100">
+            </div>	
+		</div>
+	</section>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,13 @@
 <?php
 
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\AdminController;
+use App\Http\Controllers\RegisterController;
 use App\Http\Controllers\JobCategoryController;
 use App\Http\Controllers\LandingPageController;
-use App\Http\Controllers\AuthController;
 use App\Http\Controllers\RegisterCompanieController;
-use App\Http\Controllers\RegisterController;
-use Illuminate\Support\Facades\Route;
+use Illuminate\Foundation\Auth\EmailVerificationRequest;
 
 /*
 |--------------------------------------------------------------------------
@@ -24,8 +25,22 @@ Route::get('/login-page', [AuthController::class, 'index'])
     ->name('login')->name('login');
 Route::get('/register/job-seekers', [RegisterController::class, 'index']);
 Route::get('/register/companies', [RegisterCompanieController::class, 'index']);
+Route::post('/register/proses', [RegisterCompanieController::class, 'Register']);
+Route::post('/register/job-seekers/proses', [RegisterController::class, 'Register']);
+
 Route::get('/admin', [AdminController::class, 'index']);
 Route::post('/auth', [AuthController::class, 'login']);
+
+Route::get('/email/verify/{id}/{hash}', function (EmailVerificationRequest $request) {
+    $request->fulfill();
+    return redirect('/');
+
+})->middleware(['auth', 'signed'])->name('verification.verify');
+Route::get('/email/verify', function () {
+    return view('register.verify-email');
+})->middleware('auth')->name('verification.notice');
+
+
 Route::get('/', [LandingPageController::class, 'index'])
     ->middleware('guest');
 Route::resource('/job-category', JobCategoryController::class);
