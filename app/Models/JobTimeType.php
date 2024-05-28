a10b29c6093f37d6010d31956f52a9643a232128
ulyaghani <ulyaghani27@gmail.com>
2024-05-28 23:45:10 +0700

    Job Time Type

--- app/Models/JobTimeType.php
+++ app/Models/JobTimeType.php
@@ -4,8 +4,21 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class JobTimeType extends Model
 {
     use HasFactory;
+
+    protected $guarded =[];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    use SoftDeletes;
+ 
+    	protected $table = "job_time_types";
+   	protected $dates = ['deleted_at'];
 }
