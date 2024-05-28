96a8cefcb4138205dc7422839ae0738922aa3fcb
Sajidsabila <sajid.sabila@gmail.com>
2024-05-29 00:13:03 +0700

    edit ala ala

--- database/migrations/2024_01_28_065408_create_job_time_types_table.php
+++ database/migrations/2024_01_28_065408_create_job_time_types_table.php
@@ -14,6 +14,7 @@ return new class extends Migration {
             $table->id();
             $table->string('type', 50);
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
