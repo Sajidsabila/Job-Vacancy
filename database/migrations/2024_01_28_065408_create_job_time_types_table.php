0167371c5e26f1b86b239c3ea9d6787707bca0ab
ulyaghani <ulyaghani27@gmail.com>
2024-05-29 18:49:45 +0700

    coba1

--- database/migrations/2024_01_28_065408_create_job_time_types_table.php
+++ database/migrations/2024_01_28_065408_create_job_time_types_table.php
@@ -14,34 +14,6 @@ return new class extends Migration {
             $table->id();
             $table->string('type', 50);
             $table->timestamps();
-        });
-    }
-
-    /**
-     * Reverse the migrations.
-     */
-    public function down(): void
-    {
-        Schema::dropIfExists('job_time_types');
-    }
-};
-
-<?php
-
-use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
-use Illuminate\Support\Facades\Schema;
-
-return new class extends Migration {
-    /**
-     * Run the migrations.
-     */
-    public function up(): void
-    {
-        Schema::create('job_time_types', function (Blueprint $table) {
-            $table->id();
-            $table->string('type', 50);
-            $table->timestamps();
             $table->softDeletes();
         });
     }
