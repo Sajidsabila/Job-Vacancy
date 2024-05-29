ae7c937ae69fc1fa8323426d1fd471058f5ddcd2
R.ImmanuelGiovanniItalianoPecaro <12a11.immanuel.giovanni@gmail.com>
2024-05-30 00:55:47 +0700

    immanuel

--- app/Http/Controllers/job_seeker/JobListingController.php
+++ app/Http/Controllers/job_seeker/JobListingController.php
@@ -3,12 +3,22 @@
 namespace App\Http\Controllers\job_seeker;
 
 use App\Http\Controllers\Controller;
+use App\Models\JobCategory;
+use App\Models\JobTimeType;
 use Illuminate\Http\Request;
 
 class JobListingController extends Controller
 {
     public function index()
     {
-        return view('job-seekers.job-listing');
+        $job_category=JobCategory::all();
+        $job_time =JobTimeType::all();
+        $data=([
+            "job_category"=>$job_category,
+            "job_time"=>$job_time
+        ]);
+        return view('job-seekers.job-listing',$data);
+
+
     }
 }
