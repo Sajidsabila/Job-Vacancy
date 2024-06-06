5acdd7c749c9da1356f3e1f57e2f500b5acfe4e4
Sajidsabila <sajid.sabila@gmail.com>
2024-06-06 08:08:42 +0700

    show candidate

--- resources/views/company/job/candidate-detail.blade.php
+++ resources/views/company/job/candidate-detail.blade.php
@@ -1,9 +1,83 @@
 @extends ('adminTemplate.layouts.main')
+
 @section('container')
     <div class="row">
         <div class="col-12">
             <div class="card">
-                <div class="card-header">Tentang</div>
+                <div class="card-header">Deskripsi Pelamar</div>
+                <div class="card-body">
+                    <div class="form-group">
+                        <label class="font-weight-bold">{!! $jobseeker->description !!}</label>
+                    </div>
+                </div>
             </div>
         </div>
-    @endsection
+
+        <div class="col-12">
+            <div class="card m-2">
+                <div class="card-header bg-primary text-white">
+                    <ul class="nav nav-pills card-header-pills d-flex">
+                        <li class="nav-item">
+                            <a class="nav-link active" href="#">Data Diri</a>
+                        </li>
+                    </ul>
+                </div>
+
+                <div class="card-body">
+                    <div class="form-group">
+                        <label class="font-weight-bold">First Name</label>
+                        <div>{{ $jobseeker->first_name . ' ' . $jobseeker->last_name }}</div>
+                    </div>
+                    <div class="form-group">
+                        <label class="font-weight-bold">Tanggal Lahir</label>
+                        <div>{{ $jobseeker->birth_date }}</div>
+                    </div>
+                    <div class="form-group">
+                        <label class="font-weight-bold">Nik</label>
+                        <div>{{ $jobseeker->nik }}</div>
+                    </div>
+                    <div class="form-group">
+                        <label class="font-weight-bold">Jenis Kelamin</label>
+                        <div>{{ $jobseeker->gender }}</div>
+                    </div>
+                    <div class="form-group">
+                        <label class="font-weight-bold">Agama</label>
+                        <div>{{ $jobseeker->religion->religion }}</div>
+                    </div>
+                    <div class="form-group">
+                        <label class="font-weight-bold">No Handphone</label>
+                        <div>{{ $jobseeker->phone }}</div>
+                    </div>
+                    <div class="form-group">
+                        <label class="font-weight-bold">Alamat Tempat Tinggal</label>
+                        <div>{{ $jobseeker->address }}</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-12">
+            <div class="col-6">
+                <div class="card m-2">
+                    <div class="card-header bg-primary text-white">Kemampuan</div>
+
+                    <div class="form-group ml-3">
+
+                        <ul class="list-group  m-3">
+                            @foreach ($skill as $skill)
+                                <li class="list-group-item">{{ $skill->skill }}
+                                </li>
+                            @endforeach
+
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div class="col-6">
+                <div class="card">
+                    <div class="card-header">Riwayat Pendidikan Pelamar</div>
+                    <div class="card-body"></div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
--- resources/views/company/job/detail.blade.php
+++ resources/views/company/job/detail.blade.php
@@ -2,6 +2,70 @@
 @section('container')
     <div class="row">
         <div class="col-12">
+            <div class="card">
+                <div class="card-header">{{ $title }}</div>
+                <div class="card-body">
+                    <div class="col-6">
+                        <div class="form-group">
+                            <label>Posisi</label>
+                            <input type="text" value="{{ $job->title }}" class="form-control" disabled>
+                        </div>
+                    </div>
 
+                    <div class="col-6">
+                        <div class="form-group">
+                            <label>Kategori Pekerjaan</label>
+                            <input type="text" value="{{ optional($job->jobCategory)->category }}" class="form-control"
+                                disabled>
+                        </div>
+                    </div>
+
+                    <div class="col-6">
+                        <div class="form-group">
+                            <label>Posisi</label>
+                            <input type="text" value="{{ $job->title }}" class="form-control" disabled>
+                        </div>
+                    </div>
+
+                    <div class="col-6">
+                        <div class="form-group">
+                            <label>Gaji</label>
+                            <input type="text" value="{{ $job->salary }}" class="form-control" disabled>
+                        </div>
+                    </div>
+                </div>
+                <div class="row m-3">
+                    <table id="datatable2" class="table table-striped">
+                        <thead>
+                            <tr>
+                                <th>No</th>
+                                <th>Nama Kandidat</th>
+                                <th>File Lamaran</th>
+                                <th>Status</th>
+                                <th>Aksi</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach ($jobhistoris as $key => $jobhistori)
+                                <tr>
+                                    <td>{{ $key + 1 }}</td>
+                                    <td>{{ $jobhistori->jobseeker->first_name }} {{ $jobhistori->jobseeker->last_name }}
+                                    </td>
+                                    <td>{{ $jobhistori->file }}</td>
+                                    <td>{{ $jobhistori->status }}</td>
+                                    <td>
+                                        <div class="d-flex">
+                                            <a href="{{ URL::to('/companie/lowongan-kerja/detail_candidate/' . $jobhistori->id) }}"
+                                                class="mr-2 btn btn-info btn-sm">Lihat Pelamar</a>
+                                            <a href="{{ URL::to('/companie/lowongan-kerja/' . $job->id . '/edit') }}"
+                                                class="mr-2 btn btn-warning btn-sm">Edit</a>
+                                        </div>
+                                    </td>
+                            @endforeach
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
         </div>
     @endsection
