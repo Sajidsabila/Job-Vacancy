9cf133fad572044c35b3768f0311799e644228ab
Milaa123 <jamilaturry@gmail.com>
2024-06-19 17:01:40 +0700

    breadcrumb

--- resources/views/super-admin/configuration/index.blade.php
+++ resources/views/super-admin/configuration/index.blade.php
@@ -4,15 +4,18 @@
 
     <h3> {{ $title }} </h3>
     <hr>
-    <div class="row mt-3">
+    <div class="container mt-3">
+        <div class="row">
+          
         @foreach ($configurations as $configuration)
-            <div class="col-6 ms-5">
+        <div class="col-12 col-md-6 mb-3">
                 <img src="{{ asset('storage/' . $configuration->logo) }}" alt="{{ $configuration->company_name }}"
                     width="300px">
             </div>
 
-            <div class="col-6">
-                <div class="col-12">
+            <div class="col-12 col-md-6">
+                <div class="row">
+                    <div class="col-12">
                     <div class="form-group">
                         <label for="name">Nama Perusahaan</label>
                         <input type="email" id="email" name="email" value="{{ $configuration->company_name }}"
@@ -68,6 +71,7 @@
                     Hapus
                 </button>
             </form> --}}
+            </div>
             <a href="{{ URL::to('/admin/configuration/' . $configuration->id . '/edit') }}"
                 class="btn btn-warning btn-md m-2">Edit</a>
         @endforeach
