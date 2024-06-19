26fc50ce432453b8bdd36af9f52846e2f9f5377a
Milaa123 <jamilaturry@gmail.com>
2024-06-19 08:50:13 +0700

    responsive company

--- resources/views/company/job/detail.blade.php
+++ resources/views/company/job/detail.blade.php
@@ -1,90 +1,79 @@
 @extends ('adminTemplate.layouts.main')
 @section('container')
     @include('sweetalert::alert')
-    <div class="col-12">
-        <div class="card">
-            <div class="card-header">{{ $title }}</div>
-            <div class="card-body">
-                <div class="col-6">
-                    <div class="form-group">
-                        <label>Posisi</label>
-                        <input type="text" value="{{ $job->title }}" class="form-control" disabled>
-                    </div>
-                </div>
-
-                <div class="col-6">
-                    <div class="form-group">
-                        <label>Kategori Pekerjaan</label>
-                        <input type="text" value="{{ optional($job->jobCategory)->category }}" class="form-control"
-                            disabled>
-                    </div>
-                </div>
-
-                <div class="col-6">
-                    <div class="form-group">
-                        <label>Posisi</label>
-                        <input type="text" value="{{ $job->title }}" class="form-control" disabled>
-                    </div>
-                </div>
-
-                <div class="col-6">
-                    <div class="form-group">
-                        <label>Gaji</label>
-                        <input type="text" value="{{ number_format($job->salary) }}" class="form-control" disabled>
-                    </div>
-                </div>
-            </div>
-            <div class="row m-3">
-
-                <div class="col-3">
-                    <div class="small-box bg-info">
-                        <div class="inner">
-                            <h3>{{ $countinterview }}</h3>
-                            <p>Proses Interview</p>
+     <div class="container mt-4">
+        <div class="col-12">
+            <div class="card">
+                <div class="card-header">{{ $title }}</div>
+                <div class="card-body">
+                    <div class="row">
+                        <div class="col-12 col-md-6">
+                            <div class="form-group">
+                                <label>Posisi</label>
+                                <input type="text" value="{{ $job->title }}" class="form-control" disabled>
+                            </div>
                         </div>
-                        <div class="icon">
-                            <i class="ion ion-person-add"></i>
+                        <div class="col-12 col-md-6">
+                            <div class="form-group">
+                                <label>Kategori Pekerjaan</label>
+                                <input type="text" value="{{ optional($job->jobCategory)->category }}" class="form-control" disabled>
+                            </div>
                         </div>
-                        <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+                        <div class="col-12 col-md-6">
+                            <div class="form-group">
+                                <label>Gaji</label>
+                                <input type="text" value="{{ number_format($job->salary) }}" class="form-control" disabled>
+                            </div>
+                        </div>
                     </div>
                 </div>
-
-                <div class="col-3">
-                    <div class="small-box bg-info">
-                        <div class="inner">
-                            <h3>{{ $countaccept }}</h3>
-                            <p>Lamaran Diterima</p>
+                <div class="row m-3">
+                    <div class="col-12 col-md-4">
+                        <div class="small-box bg-info">
+                            <div class="inner">
+                                <h3>{{ $countinterview }}</h3>
+                                <p>Proses Interview</p>
+                            </div>
+                            <div class="icon">
+                                <i class="ion ion-person-add"></i>
+                            </div>
+                            <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
                         </div>
-                        <div class="icon">
-                            <i class="ion ion-person-add"></i>
+                    </div>
+                    <div class="col-12 col-md-4">
+                        <div class="small-box bg-info">
+                            <div class="inner">
+                                <h3>{{ $countaccept }}</h3>
+                                <p>Lamaran Diterima</p>
+                            </div>
+                            <div class="icon">
+                                <i class="ion ion-person-add"></i>
+                            </div>
+                            <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
                         </div>
-                        <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+                    </div>
+                    <div class="col-12 col-md-4">
+                        <div class="small-box bg-danger">
+                            <div class="inner">
+                                <h3>{{ $countreject }}</h3>
+                                <p>Lamaran Ditolak</p>
+                            </div>
+                            <div class="icon">
+                                <i class="ion ion-person-add"></i>
+                            </div>
+                            <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+                        </div>
                     </div>
                 </div>
-
-                <div class="col-3">
-                    <div class="small-box bg-danger">
-                        <div class="inner">
-                            <h3>{{ $countreject }}</h3>
-                            <p>Lamaran Ditolak</p>
-                        </div>
-                        <div class="icon">
-                            <i class="ion ion-person-add"></i>
-                        </div>
-                        <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
-                    </div>
-                </div>
-                <div class="col-12">
-                    <div class="col-3">
+                <div class="row m-3">
+                    <div class="col-12">
                         <form action="" method="GET">
                             <div class="form-group">
                                 <label for="statusFilter">Filter</label>
                                 <select name="statusFilter" id="statusFilter" class="form-control">
-                                    <option value="" {{ is_null($selectedStatus) ? 'selected' : '' }}>Semua Status
-                                    </option>
+                                    <option value="" {{ is_null($selectedStatus) ? 'selected' : '' }}>Semua Status</option>
                                     @foreach ($statuses as $status)
-                                        <option value="{{ $status->status }}"
-                                            {{ $selectedStatus == $status->status ? 'selected' : '' }}>
+                                        <option value="{{ $status->status }}" {{ $selectedStatus == $status->status ? 'selected' : '' }}>
                                             {{ $status->status }}
                                         </option>
                                     @endforeach
@@ -94,65 +83,61 @@
                         </form>
                     </div>
                 </div>
-                <table id="datatable2" class="table table-striped">
-                    <thead>
-                        <tr>
-                            <th>No</th>
-                            <th>Nama Kandidat</th>
-                            <th>File Lamaran</th>
-                            <th>Status</th>
-                            <th>Tangga Interview</th>
-                            <th>Aksi</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        @foreach ($jobhistoris as $key => $jobhistori)
-                            <tr>
-                                <td>{{ $key + 1 }}</td>
-                                <td>{{ $jobhistori->jobseeker->first_name }}
-                                    {{ $jobhistori->jobseeker->last_name }}
-                                </td>
-                                <td><a href="{{ URL::to('/companie/lowongan-kerja/view-pdf/' . $jobhistori->id) }}">Lihat
-                                        Lamaran</a></td>
-                                <td>
-                                    @if ($jobhistori->status == 'Lamaran Terkirim')
-                                        <span class="badge badge-primary">{{ $jobhistori->status }}</span>
-                                    @elseif($jobhistori->status == 'Lamaran Dilihat')
-                                        <span class="badge badge-info">{{ $jobhistori->status }}</span>
-                                    @elseif($jobhistori->status == 'Lamaran Ditolak')
-                                        <span class="badge badge-danger">{{ $jobhistori->status }}</span>
-                                    @else
-                                        <span class="badge badge-success">{{ $jobhistori->status }}</span>
-                                    @endif
-                                </td>
-                                <td>
-                                    @if (!is_null($jobhistori->interview_date))
-                                        {{ formatIndonesianDate($jobhistori->interview_date) }}
-                                    @else
-                                        Tanggal Interview Disetting
-                                    @endif
-                                </td>
-                                <td>
-                                    <div class="d-flex">
-                                        <a href="{{ URL::to('/companie/lowongan-kerja/detail_candidate/' . $jobhistori->id) }}"
-                                            class="mr-2 btn btn-info btn-sm">Lihat Pelamar</a>
-                                        @if ($jobhistori->status == 'Lamaran Dilihat' || $jobhistori->status == 'Proses Interview')
-                                            <a href="{{ URL::to('/companie/lowongan-kerja/set-interview/' . $jobhistori->id) }}"
-                                                class="mr-2 btn btn-warning btn-sm">Interview</a>
-                                        @endif
-
-                                        @if (!is_null($jobhistori->interview_date))
-                                            <a href="{{ URL::to('/companie/lowongan-kerja/reject/' . $jobhistori->id) }}"
-                                                class="mr-2 btn btn-danger btn-sm">Tolak Lamaran</a>
-                                            <a href="{{ URL::to('/companie/lowongan-kerja/accept/' . $jobhistori->id) }}"
-                                                class="mr-2 btn btn-success btn-sm">Terima Lamaran</a>
-                                        @endif
-                                    </div>
-                                </td>
-                        @endforeach
-                        </tr>
-                    </tbody>
-                </table>
+                <div class="row m-3">
+                    <div class="col-12">
+                        <table id="datatable2" class="table table-striped">
+                            <thead>
+                                <tr>
+                                    <th>No</th>
+                                    <th>Nama Kandidat</th>
+                                    <th>File Lamaran</th>
+                                    <th>Status</th>
+                                    <th>Tanggal Interview</th>
+                                    <th>Aksi</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                @foreach ($jobhistoris as $key => $jobhistori)
+                                    <tr>
+                                        <td>{{ $key + 1 }}</td>
+                                        <td>{{ $jobhistori->jobseeker->first_name }} {{ $jobhistori->jobseeker->last_name }}</td>
+                                        <td><a href="{{ URL::to('/companie/lowongan-kerja/view-pdf/' . $jobhistori->id) }}">Lihat Lamaran</a></td>
+                                        <td>
+                                            @if ($jobhistori->status == 'Lamaran Terkirim')
+                                                <span class="badge badge-primary">{{ $jobhistori->status }}</span>
+                                            @elseif($jobhistori->status == 'Lamaran Dilihat')
+                                                <span class="badge badge-info">{{ $jobhistori->status }}</span>
+                                            @elseif($jobhistori->status == 'Lamaran Ditolak')
+                                                <span class="badge badge-danger">{{ $jobhistori->status }}</span>
+                                            @else
+                                                <span class="badge badge-success">{{ $jobhistori->status }}</span>
+                                            @endif
+                                        </td>
+                                        <td>
+                                            @if (!is_null($jobhistori->interview_date))
+                                                {{ formatIndonesianDate($jobhistori->interview_date) }}
+                                            @else
+                                                Tanggal Interview Disetting
+                                            @endif
+                                        </td>
+                                        <td>
+                                            <div class="d-flex">
+                                                <a href="{{ URL::to('/companie/lowongan-kerja/detail_candidate/' . $jobhistori->id) }}" class="mr-2 btn btn-info btn-sm">Lihat Pelamar</a>
+                                                @if ($jobhistori->status == 'Lamaran Dilihat' || $jobhistori->status == 'Proses Interview')
+                                                    <a href="{{ URL::to('/companie/lowongan-kerja/set-interview/' . $jobhistori->id) }}" class="mr-2 btn btn-warning btn-sm">Interview</a>
+                                                @endif
+                                                @if (!is_null($jobhistori->interview_date))
+                                                    <a href="{{ URL::to('/companie/lowongan-kerja/reject/' . $jobhistori->id) }}" class="mr-2 btn btn-danger btn-sm">Tolak Lamaran</a>
+                                                    <a href="{{ URL::to('/companie/lowongan-kerja/accept/' . $jobhistori->id) }}" class="mr-2 btn btn-success btn-sm">Terima Lamaran</a>
+                                                @endif
+                                            </div>
+                                        </td>
+                                    </tr>
+                                @endforeach
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
--- resources/views/company/profil-company/index.blade.php
+++ resources/views/company/profil-company/index.blade.php
@@ -70,54 +70,55 @@
     </section>
     <h3> {{ $title }} </h3>
     <hr>
-    <div class="row mt-3">
-        <div class="col-6 ms-5">
-            <img src="{{ URL::to('storage/' . $company->logo) }}" alt="{{ $company->company_name }}" width="300px">
+    <div class="container mt-3">
+        <div class="row">
+            <div class="col-12 col-md-6 mb-3">
+                <img src="{{ URL::to('storage/' . $company->logo) }}" alt="{{ $company->company_name }}" class="img-fluid">
+            </div>
+
+            <div class="col-12 col-md-6">
+                <div class="row">
+                    <div class="col-12">
+                        <div class="form-group">
+                            <label for="name">Nama Perusahaan</label>
+                            <input type="text" id="email" name="email" value="{{ $company->company_name }}" class="form-control @error('email') is-invalid @enderror" readonly>
+                        </div>
+                    </div>
+
+                    <div class="col-12">
+                        <div class="form-group">
+                            <label for="name">Deskripsi Perusahaan</label>
+                            {!! $company->deskripsi !!}
+                        </div>
+                    </div>
+
+                    <div class="col-12">
+                        <div class="form-group">
+                            <label for="name">Email Perusahaan</label>
+                            <input type="email" id="email" name="email" value="{{ $company->email }}" class="form-control @error('email') is-invalid @enderror" readonly>
+                        </div>
+                    </div>
+
+                    <div class="col-12">
+                        <div class="form-group">
+                            <label for="name">Nomor Telepon Perusahaan</label>
+                            <input type="text" id="email" name="email" value="{{ $company->phone }}" class="form-control @error('email') is-invalid @enderror" readonly>
+                        </div>
+                    </div>
+
+                    <div class="col-12">
+                        <div class="form-group">
+                            <label for="name">Alamat Perusahaan</label>
+                            <input type="text" id="email" name="email" value="{{ $company->address }}" class="form-control @error('email') is-invalid @enderror" readonly>
+                        </div>
+                    </div>
+
+                    <div class="col-12">
+                        <a href="{{ URL::to('/companie/company-profile/' . $company->id . '/edit') }}" class="btn btn-warning btn-md mt-2">Edit</a>
+                    </div>
+                </div>
+            </div>
         </div>
-
-        <div class="col-6">
-            <div class="col-12">
-                <div class="form-group">
-                    <label for="name">Nama Perusahaan</label>
-                    <input type="email" id="email" name="email" value="{{ $company->company_name }}"
-                        class="form-control @error('email')is-invalid @enderror" readonly>
-                </div>
-            </div>
-
-            <div class="col-12">
-                <div class="form-group">
-                    <label for="name">Deskripsi Perusahaan</label>
-                    {!! $company->deskripsi !!}
-                </div>
-            </div>
-
-            <div class="col-12">
-                <div class="form-group">
-                    <label for="name">Email Perusahaan</label>
-                    <input type="email" id="email" name="email" value="{{ $company->email }}"
-                        class="form-control @error('email')is-invalid @enderror" readonly>
-                </div>
-            </div>
-
-            <div class="col-12">
-                <div class="form-group">
-                    <label for="name">Nomer Telpon Perusahaan</label>
-                    <input type="email" id="email" name="email" value="{{ $company->phone }}"
-                        class="form-control @error('email')is-invalid @enderror" readonly>
-                </div>
-            </div>
-
-            <div class="col-12">
-                <div class="form-group">
-                    <label for="name">Alamat Perusahaan</label>
-                    <input type="email" id="email" name="email" value="{{ $company->addres }}"
-                        class="form-control @error('email')is-invalid @enderror" readonly>
-                </div>
-            </div>
-
-        </div>
-        <a href="{{ URL::to('/companie/company-profile/' . $company->id . '/edit') }}"
-            class="btn btn-warning btn-md m-2">Edit</a>
-
     </div>
+   
 @endsection
